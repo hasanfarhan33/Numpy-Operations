8ce8e7f45d938f174d3747b6f6511d0dd151f8f1
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-11 15:44:32 +0300

    Final Commit

--- numpyOperations.py
+++ numpyOperations.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+arr = np.arange(0, 11)
+print(arr)
+
+print(arr + arr)
+print(arr - arr)
+print(arr + 100)
+print(arr * 4)
+print(arr - 100)
+print(arr / arr)
+print(1/arr)
+print(arr ** 2)
+print(np.sqrt(arr))
+print(np.exp(arr))
+print(np.max(arr))
+print(arr.max())
+print(np.sin(arr))
